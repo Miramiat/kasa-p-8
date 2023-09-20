a03a1d577b2445874e788a2127d90b87af7524a7
AZIZ <124620051+Miramiat@users.noreply.github.com>
2023-09-21 00:40:51 +0200

    first commit

---
+++ src/components/Slide.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import '../styles/Slide.scss';
+
+function Slide({ images }) {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentImage((prevImage) => (prevImage - 1 + images.length) % images.length);
+  };
+
+  return (
+    <div className="slide">
+      <button className="prev-arrow" onClick={prevSlide}>
+        &#8249;
+      </button>
+      <img
+        src={images[currentImage]}
+        alt={`Image ${currentImage}`}
+        className="slide-image"
+      />
+      <button className="next-arrow" onClick={nextSlide}>
+        &#8250;
+      </button>
+    </div>
+  );
+}
+
+export default Slide;
+
+
